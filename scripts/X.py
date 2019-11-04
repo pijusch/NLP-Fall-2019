bc9ebd8485d058a7a429992af60632f24fa6e848
Piyush Chawla <pijusch@Piyushs-MacBook-Pro.local>
2019-11-03 23:06:38 -0500

    model X added

--- scripts/X.py
+++ scripts/X.py
@@ -1,4 +1,5 @@
 import sys
+import random
 from input_transE import input_transe
 import numpy as np
 import torch
@@ -6,17 +7,20 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader, TensorDataset
-from input_transE import input_transe
+from input_X import input_transe
 from hits_eval import HitsEval
 
 class TransE(nn.Module):
-    def __init__(self, data, DIM_EMB=100):
+    def __init__(self, data, DIM_EMB=100, DIM_LSTM=100):
         super(TransE, self).__init__()
         self.dim = DIM_EMB
         self.num_ent = len(data.ent_dic)
         self.num_rel = len(data.rel_dic)
-        self.ent_embedding = nn.Embedding(len(data.ent_dic),DIM_EMB)
-        self.rel_embedding = nn.Embedding(len(data.rel_dic),DIM_EMB)
+        self.sub_lstm = nn.LSTM(DIM_LSTM,DIM_EMB)
+        self.obj_lstm = nn.LSTM(DIM_LSTM,DIM_EMB)
+        self.rel_lstm = nn.LSTM(DIM_LSTM,DIM_EMB)
+        self.ent_embedding = nn.Embedding(len(data.ent_dic)+1,DIM_EMB)
+        self.rel_embedding = nn.Embedding(len(data.rel_dic)+1,DIM_EMB)
         self.embeddings = [self.ent_embedding, self.rel_embedding]
         self.initialize_embeddings()
         self.cuda(0)
@@ -35,6 +39,12 @@ class TransE(nn.Module):
         sub = self.ent_embedding(subjects)
         obj = self.ent_embedding(objects)
         rel = self.rel_embedding(relations)
+        sub, _ = self.sub_lstm(sub.squeeze(1))
+        obj, _ = self.obj_lstm(obj.squeeze(1))
+        rel, _ = self.rel_lstm(rel.squeeze(1))
+        sub = torch.mean(sub,1)
+        obj = torch.mean(obj,1)
+        rel = torch.mean(rel,1)
         score = torch.sum((sub+rel-obj)**2,-1)
         #score = torch.mul(score,score)
         #score = torch.sum(score,-1)
@@ -42,14 +52,16 @@ class TransE(nn.Module):
 
 def transe_epoch(spo):
     s, o, p = torch.chunk(spo, 3, dim=1)
-    no = torch.LongTensor(np.random.randint(0, model.num_ent, o.size(0))).view(-1,1).cuda(0)
-    ns = torch.LongTensor(np.random.randint(0, model.num_ent, s.size(0))).view(-1,1).cuda(0)
+    no, ns = neg_gen()
+    #no = torch.LongTensor(np.random.randint(0, model.num_ent, o.size(0))).view(-1,1).cuda(0)
+    #ns = torch.LongTensor(np.random.randint(0, model.num_ent, s.size(0))).view(-1,1).cuda(0)
     criterion = lambda pos, neg: torch.sum(torch.max(Variable(zero), 1 + pos - neg))
     
     optimizer.zero_grad()
     pos_score = model(Variable(ns),Variable(o),Variable(p))
     neg_score = model(Variable(s),Variable(o),Variable(p))
     loss = criterion(pos_score,neg_score)
+    #loss = torch.sum(pos_score)
     loss.backward()
     optimizer.step()
 
@@ -64,13 +76,13 @@ def transe_epoch(spo):
 
 def train_transe(data, n_iter):
     print("Start Training!")
-    tensor_spo = torch.LongTensor(data.train).cuda(0)
-    train_dataset = DataLoader(TensorDataset(tensor_spo, torch.zeros(tensor_spo.size(0))), batch_size=4196, shuffle=True, drop_last=True)
+    tensor_spo = torch.LongTensor(data.test),cuda(0)
+    train_dataset = DataLoader(TensorDataset(tensor_spo, torch.zeros(tensor_spo.size(0))), batch_size=batch_n, shuffle=True, drop_last=True)
     for epoch in range(n_iter):
         total_loss = 0.0
         for batch_id, (spo, _) in enumerate(train_dataset):
             loss = transe_epoch(spo)
-            #print(batch_id,loss)
+            print(batch_id,loss)
             total_loss += loss
         print(f"loss on epoch {epoch} = {total_loss}")
     return
@@ -78,8 +90,8 @@ def train_transe(data, n_iter):
 def eval(data):
     emb = []
     c = 0
-    entities = model.ent_embedding(torch.LongTensor(range(model.num_ent)).cuda(0)).cpu().detach().numpy()
-    relations = model.rel_embedding(torch.LongTensor(range(model.num_rel)).cuda(0)).cpu().detach().numpy()
+    entities = model.ent_embedding(torch.LongTensor(range(model.num_ent))).detach().numpy()
+    relations = model.rel_embedding(torch.LongTensor(range(model.num_rel))).detach().numpy()
     for i in data.train.astype(int):
         t = []
         t.append(entities[i[0]])
@@ -90,14 +102,20 @@ def eval(data):
     he = HitsEval(emb,data.train.astype(int),entities,relations)
     print(he.relations())
 
+def neg_gen():
+    ns = torch.LongTensor(np.array(random.sample(data.train,batch_n))[:,0]).cuda(0)
+    no = torch.LongTensor(np.array(random.sample(data.train,batch_n))[:,1]).cuda(0)
+    return [ns,no]
+
 if __name__ == "__main__":
+    batch_n = 4196
     data = input_transe()
     model = TransE(data)
     optimizer = optim.Adam(model.parameters())
     zero = torch.FloatTensor([0.0]).cuda(0)
-    #train_transe(data,50)
-    #torch.save(model,'./transE.model')
-    model = torch.load('./transE.model')
+    train_transe(data,50)
+    torch.save(model,'./transE.model')
+    #model = torch.load('./transE.model')
     #model.nerwork.cpu()
-    eval(data)
+    #eval(data)
     #print(model.ent_embedding(torch.LongTensor([1])))
---
+++ scripts/input_X.py
@@ -0,0 +1,73 @@
+import pandas as pd 
+import numpy as np
+import pickle
+import random
+import os
+
+def get_labels(dic,x,num):
+    re = []
+    for i in x.split():
+        re.append(dic[i])
+    if len(re)>num:
+        re = re[:num]
+    elif len(re)<num:
+        for i in range(len(re),num):
+            re.append(len(dic))
+    return re
+
+class input_transe:
+    def __init__(self):
+        with open('../data/entities_relations.pkl','rb') as f:
+            self.ent_rel = pickle.load(f)
+        self.ent_dic = dict()
+        self.rel_dic = dict()
+        self.train = []
+        self.test = []
+        self.valid = []
+        self.input()
+    
+    def input(self):
+        if 'Xinput.pkl' in os.listdir():
+            with open('Xinput.pkl','rb') as f:
+                temp = pickle.load(f)
+                self.train = temp[0]
+                self.test = temp[1]
+                self.valid = temp[2]
+                self.rel_dic = temp[3]
+                self.ent_dic = temp[4]
+            return
+        data = pd.read_csv('../data/total.csv',sep='\t')
+        
+        for i in range(len(self.ent_rel[0])):
+            self.ent_dic[i] = self.ent_rel[0][i]
+            self.ent_dic[self.ent_rel[0][i]] = i
+        
+        for i in range(len(self.ent_rel[1])):
+            self.rel_dic[i] = self.ent_rel[1][i]
+            self.rel_dic[self.ent_rel[1][i]] = i
+
+        out = []
+        for i in range(len(data)):
+            tmp = []
+            tmp.append(get_labels(self.ent_dic,data['0'].iloc[i],4))
+            tmp.append(get_labels(self.ent_dic,data['1'].iloc[i],4))
+            tmp.append(get_labels(self.rel_dic,data['2'].iloc[i],3))
+            out.append(tmp)
+        n = len(out)
+        self.train = out[0:int(n*0.8)]
+        self.test = out[int(n*0.8):int(n*0.9)]
+        self.valid = out[int(n*0.9):]
+        with open('Xinput.pkl','wb') as f:
+            pickle.dump([self.train,self.test,self.valid,self.rel_dic,self.ent_dic],f)
+        
+
+
+    def get_id(self,name,dic):
+        return dic[name]
+    
+    def get_name(self,id,dic):
+        return dic[id]
+    
+if __name__ == '__main__':
+    n = input_transe()
+    print(n.train[0])
